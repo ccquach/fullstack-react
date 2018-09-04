ed7a8c8603521f4b45926ef57ea6f674ab853c6f
cynthia.quach <ccquach93@gmail.com>
2018-09-04 10:02:47 -0700

    refactor server folder structure

--- server/index.js
+++ server/index.js
@@ -1,34 +1,12 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
+// CONFIG
+require('./services/passport');
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+// ROUTES
+require('./routes/authRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`Serving emailer app on port ${PORT}`);
