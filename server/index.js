dca603bc3d3bc9ed42860eea37779f290041c3a2
cynthia.quach <ccquach93@gmail.com>
2018-09-04 22:08:33 -0700

    connect to mongo db and add cookie sessions

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+// DB CONFIG
+require('./models/User');
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true }
+);
+
+// AUTH CONFIG
+require('./services/passport');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// CONFIG
-require('./services/passport');
+// MIDDLEWARE
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // ROUTES
 require('./routes/authRoutes')(app);
