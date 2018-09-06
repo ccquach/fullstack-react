9a560c33aa1fb8cb20eca2698b733eed619a169c
cynthia.quach <ccquach93@gmail.com>
2018-09-06 11:31:14 -0700

    hook up auth state to header

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions/';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyForm = () => <h2>SurveyForm</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <Router>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyForm} />
-        </div>
-      </Router>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount = () => {
+    this.props.fetchUser();
+  };
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyForm} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
