3c11ed19f533f90b1bae3eb868070d9c82c9d791
cynthia.quach <ccquach93@gmail.com>
2018-09-06 09:30:09 -0700

    add react routing structure, add materialize for styles, create Header component

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyForm = () => <h2>SurveyForm</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="container">
+      <Router>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyForm} />
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
